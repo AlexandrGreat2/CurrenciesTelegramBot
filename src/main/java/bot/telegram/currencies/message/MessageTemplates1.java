4e2b004c3ad6af069975e8f5eede07529727d529
Oksana Svishcho <oksana.svishcho@gmail.com>
2024-03-19 19:44:57 +0200

    message - init commit message - init commit

--- src/main/java/bot/telegram/currencies/message/MessageTemplates1.java
+++ src/main/java/bot/telegram/currencies/message/MessageTemplates1.java
@@ -2,7 +2,7 @@ package bot.telegram.currencies.message;
 
 public class MessageTemplates1 {
     public static final String GREETING_TEMPLATE = "Привіт, %s! Цей бот допоможе тобі відслідковувати актуальні курси валют!";
-    public static final String FAREWELL_TEMPLATE = "Допобачення, %s!";
+    public static final String FAREWELL_TEMPLATE = "Допобачення!";
 
     public static String getGreetingMessage(String username) {
         return String.format(GREETING_TEMPLATE, username);
